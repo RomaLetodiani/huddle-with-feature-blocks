e49c9dcb7c03af9b0bab8ea9bfd6e8d0e0af5a96
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-06 00:31:33 +0400

    built header component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,18 @@
+import Button from '../UI/Button';
+import { GlobalObj } from '../components/shared/consts';
+
 const Header = () => {
-  return <div>Header</div>;
+  const { header } = GlobalObj;
+  const { logo } = GlobalObj;
+  return (
+    <div className="flex justify-between items-center gap-2 sm:gap-10 py-4">
+      <img className="w-32 sm:w-auto" src={logo} alt="logo" />
+      <Button
+        className="shadow-lg hover:opacity-70 bg-white rounded-full px-4 sm:px-8 py-1 sm:py-2 text-xs sm:text-base text-nowrap text-Very-Dark-Cyan"
+        text={header.button}
+      />
+    </div>
+  );
 };
 
 export default Header;
