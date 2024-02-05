ad89109d1846fc4f9467ff98d38dd3799718b45e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-06 00:32:01 +0400

    built footer component

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,50 @@
+import SectionWrapper from '../components/SectionWrapper';
+import { GlobalObj } from '../components/shared/consts';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const { footer, socials, logo } = GlobalObj;
+  return (
+    <div className="py-20 text-white bg-Very-Dark-Cyan">
+      <SectionWrapper>
+        <img className="invert brightness-0 mb-10" src={logo} alt="logo" />
+        <div className="flex flex-wrap gap-20 justify-center md:justify-between">
+          <ul className="flex flex-col gap-10">
+            {footer.left.map((item, index) => (
+              <li className="flex items-center gap-3" key={index}>
+                <img src={item.icon} alt={item.alt} />
+                {item.text}
+              </li>
+            ))}
+          </ul>
+          {footer.middle.map((item, index) => (
+            <div key={index} className="flex flex-col ">
+              <ul className="flex flex-col gap-5">
+                {item.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className="flex flex-col gap-10 items-center lg:items-start justify-between">
+            <div className="flex gap-3">
+              {socials.map((item, index) => (
+                <a key={index} href={item.link}>
+                  <div className="cursor-pointer border border-white rounded-full p-3">
+                    <img
+                      className="invert brightness-0 w-6 h-6"
+                      src={item.icon}
+                      alt={item.alt}
+                    />
+                  </div>
+                </a>
+              ))}
+            </div>
+            <p className="text-xs md:text-sm">{footer.copyright}</p>
+          </div>
+        </div>
+      </SectionWrapper>
+    </div>
+  );
 };
 
 export default Footer;
